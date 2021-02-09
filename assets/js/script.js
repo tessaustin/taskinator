9e418b349e9592ac9886096b7a9b0ea2769edf2a
Tess Austin <tessaustin92@gmail.com>
2021-02-09 16:00:28 -0700

    task add on working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() { 
-  var listItemEl = document.createElement("li"); 
+var createTaskHandler = function(event) { 
+    event.preventDefault();
+
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    console.log(taskTypeInput);
+
+  var listItemEl = document.createElement("li");
   listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
+
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  tasksToDoEl.appendChild(listItemEl);
   }; 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
